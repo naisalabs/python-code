dacbeb30e94dd29001d80995e5185b0c5b96fc20
naisalabs <mewtimebd@gmail.com>
2026-03-18 23:28:13 +0600

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,13 @@
+#creating differemt variables
+x=5
+y= "john"
+print(x)
+print(y)
+    
+codingal="codingal"
+print(codingal)
+
+#user input
+name = input("enter your full name:")
+
+print("\nhello",name, "\nwelcome to codingal")
