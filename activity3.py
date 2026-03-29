c15f40686f1bf2f5a735774b6273e94ccb347cc3
naisalabs <mewtimebd@gmail.com>
2026-03-29 23:36:06 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,5 +1,12 @@
-str1 = 'Naisa'
-str2 = 'Labs'
+#taking total amount as input from user
+amount =int(input("pls enter amount for withraw:"))
 
-# using +
-print('str1 + str2 =',str1 and str2)
+#calculating the number of notes of different denominations
+note_1 = amount//200
+note_2= (amount%100)//50
+note_3 = ((amount%100)%50)//10
+
+
+print("notes if 200 taka" ,note_1)
+print("notes of 50 taka" , note_2)
+print("notes of 10 taka" , note_3)
