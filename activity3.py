c7499e4dbc39e3b6d449904dcc3de61b8c122f42
naisalabs <mewtimebd@gmail.com>
2026-03-29 22:52:42 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,26 +1,5 @@
-#assigning different variebles
-name = "penguin"
-age = 67
-is_student= True
-weight = 67.6
+str1 = 'Naisa'
+str2 = 'Labs'
 
-#printing different variebles and their data type
-print("Name :",name)
-print("data type of name is" ,type(name))
-print("age :",age)
-print("data type of age is" ,type(age))
-
-print("is_student :",is_student)
-print("data type of is_student is" ,type(is_student))
-
-print("is_student :",weight)
-print("data type of weight is" ,type(weight))
-
-#casting to convert the datatype of variebles
-print("\n after type casting ....")
-age = sty(age)
-print(age)
-print("data type of age is", type(age))
-weight = int(weight)
-print =(weight)
-print("data type of weight is",type(weight))
+# using +
+print('str1 + str2 =',str1 and str2)
