ad20827d57d6b63431916718a8c27c5e74074878
naisalabs <mewtimebd@gmail.com>
2026-03-29 23:48:05 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,12 +1,14 @@
-#taking total amount as input from user
-amount =int(input("pls enter amount for withraw:"))
+#take marks as input from user
+print("enter marks obtained in 4 subjects: ")
+history = int(input("history :"))
+english = int(input("english :"))
+science = int(input("science :"))
+bengali = int(input("bengali :"))
 
-#calculating the number of notes of different denominations
-note_1 = amount//200
-note_2= (amount%100)//50
-note_3 = ((amount%100)%50)//10
+#lets calculate the perc of marks
+sum = history+english+science+bengali
+print("sum of history,english,science and bengali")
 
+perc = (sum/400)*100
 
-print("notes if 200 taka" ,note_1)
-print("notes of 50 taka" , note_2)
-print("notes of 10 taka" , note_3)
+print("percentage mark = ",perc)
