7e2c6c55f14072b2b141ddb2bb32bac49bc20435
naisalabs <mewtimebd@gmail.com>
2026-04-01 23:18:25 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,15 +1,8 @@
-# storing values
-tree1 = 67
-tree2 = 10
-tree3 = 76
-tree4 = 7
-tree5 = 42
-tree6 = 32
+num = 3
+if num > 0:
+   print(num,"is a positive number.")
 
-#finding the total of trees
-sum = tree1+tree2+tree3+tree4+tree5+tree6
-print("the sum of all the 6 trees is: ",sum)
 
-#finding the average of trees
-average =sum/6
-print("the average of all the trees is: ",average)
+num = -1
+if num < 0:
+   print(num,"is a negtive number")
