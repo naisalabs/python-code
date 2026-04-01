48e490d02d447cc9589e69d5ac2d09aaabfbd3cd
naisalabs <mewtimebd@gmail.com>
2026-04-01 23:28:36 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,14 +1,8 @@
-#take marks as input from user
-print("enter marks obtained in 4 subjects: ")
-history = int(input("history :"))
-english = int(input("english :"))
-science = int(input("science :"))
-bengali = int(input("bengali :"))
+actual_cost = float(input("please enter the actual product price: "))
+sale_amount = float(input("please enter your sales amount: "))
 
-#lets calculate the perc of marks
-sum = history+english+science+bengali
-print("sum of history,english,science and bengali")
-
-perc = (sum/400)*100
-
-print("percentage mark = ",perc)
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("total profit",amount)
+else:
+    print("no profit!!!")
