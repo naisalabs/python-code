a18d548ab1888f34123565ea8ea9a147ce7fe522
naisalabs <mewtimebd@gmail.com>
2026-04-05 23:23:48 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,8 +1,21 @@
-actual_cost = float(input("please enter the actual product price: "))
-sale_amount = float(input("please enter your sales amount: "))
-
-if (sale_amount > actual_cost):
-    amount = sale_amount - actual_cost
-    print("total profit",amount)
+a = 10
+b = 12
+c = 1
+if a and b and c:
+    print("all the numbers have boolean values as true")
 else:
-    print("no profit!!!")
+    print("atleast one number has boolean value as false")
+
+    a = 10
+    b = -10
+    c = 0
+
+if a > 0 or b> 0:
+        print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if b > 0 or c >0:
+        print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
