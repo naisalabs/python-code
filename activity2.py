6110bc6755ec08fac61aeab2501fd47063668582
naisalabs <mewtimebd@gmail.com>
2026-04-08 23:28:18 +0600

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,18 +1,23 @@
-a = 10
-b = 12
-c = 12
-print("a != b")#true
-print("b != c")#false
-a = "python"
-b = "coding"
-if a != b :
-    print(a, 'and', b, 'are different')#its print
-a = 4
-b = 5
-if (a == 1) != (b == 5): #false != true=false
-    print('hello')#its print
+a = 12
+b = 10
+c = 0
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast one number has boiolean value as false ")
 
-    a = int(input("enter a number"))
 
-    if a%2 != 0 :
-        print(a, "its not even number")
+    a = 10
+    b = -10
+    c = 0
+
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if b > 0 or c > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
