3528c773ccdb62f85a15ae4f7b0b836530ecffbc
naisalabs <mewtimebd@gmail.com>
2026-04-15 23:49:41 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,9 +1,32 @@
-print("enter a number (numerator): ")
-numn = int(input())
-print("enter a number (denominator): ")
-numd = int(input())
+print("select your ride")
+print("1. bike")
+print("2. car")
 
-if numn%numd==0:
-    print("\n" +str(numn)+ " is divisible by " +str(numd))
+
+choice = int(input("enter your choice: "))
+
+if( choice == 1 ):   #condition 1 outer if statement
+    print("what type of bike? ")
+    print("scooty/n")
+    print("scooter/n")
+
+    choice2=int(input("enter your choice2: "))
+    if choice2==1: 
+        print("you have selected scooty")
+    else:
+        print("you have selected scooter")
+
+elif(choice == 2 ):
+          print("what type of car?")
+          print("1.sedan")
+          print("2.xuv")
+          choice3=int(input("enter your choice3"))
+          if choice3==1:
+                
+             print("you have selected sedan")
+          else:
+               print("you have selected xuv")
 else:
-    print("\n" +str(numn)+ " is divisible by " +str(numd))
+     print("wrong choice")
+               
+
