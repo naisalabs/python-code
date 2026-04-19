944bea31d4c10380a14d08fc7c9d602f16682c2c
naisalabs <mewtimebd@gmail.com>
2026-04-19 23:28:01 +0600

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,14 +1,9 @@
-# take input for the student that he ca attend the exam or not
-medical_cause = input("did you have a medical cause (y/n):").strip(). upper()
+string = input("please enter your own string: ")
 
+string2 = ('')
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
 
-if medical_cause == 'Y':   # condition 1
-   print("you're allowed")
-else:
-   
-   atten = int(input("enter the attendence of the student: "))
-
-   if atten >= 75:   #condition 2
-       print("you're allowed")
-   else:
-        print("you're not allowed")
+    print("/nthe original string = ", string)
+    print("the reversed string =", string2)
