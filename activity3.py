ae1ee3249fc91e2c904aa901e513124e3d9c864f
naisalabs <mewtimebd@gmail.com>
2026-04-19 23:38:35 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,32 +1,9 @@
-print("select your ride")
-print("1. bike")
-print("2. car")
+#input number greater than 1
+n = int(input("enter the value of n: "))
 
+#print the numbers from n to 1
+print("numbers from", n," to " ,1," are :")
 
-choice = int(input("enter your choice: "))
-
-if( choice == 1 ):   #condition 1 outer if statement
-    print("what type of bike? ")
-    print("scooty/n")
-    print("scooter/n")
-
-    choice2=int(input("enter your choice2: "))
-    if choice2==1: 
-        print("you have selected scooty")
-    else:
-        print("you have selected scooter")
-
-elif(choice == 2 ):
-          print("what type of car?")
-          print("1.sedan")
-          print("2.xuv")
-          choice3=int(input("enter your choice3"))
-          if choice3==1:
-                
-             print("you have selected sedan")
-          else:
-               print("you have selected xuv")
-else:
-     print("wrong choice")
-               
-
+#loop to print numbers
+for i in range(n,0,-1):
+    print(i)
