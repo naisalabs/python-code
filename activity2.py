1507252229cf3d1ef37bb0156e94b1be634d42b3
naisalabs <mewtimebd@gmail.com>
2026-04-26 23:28:32 +0600

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,3 +1,11 @@
-i=0
-while i<=0:
-  print("I WILL RUN FOREVER")
+string = input("please enter your own word: ")
+char = input("please enter your own chaaracter: ")
+i = 0
+count = 0
+while(i < len(string)):
+    
+    if (string[i] == char):
+        count = count + 1
+    i = i + 1
+
+print("the total number of times", char,"has occured = ", count) 
