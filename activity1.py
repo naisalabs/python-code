78b659414f7f49878d36a9a102a3955434215f2c
naisalabs <mewtimebd@gmail.com>
2026-04-26 18:39:27 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,8 +1,15 @@
-#input an integer value
-n = int(input("enter the number whose sum you want to find: "))
-sum=0
-
-#iterates for n+1 times: i=1 to n+1
-for i in range(1, n+1):
-  sum = sum+1
-  print("/nsum=", sum)
+# take input from the user
+num = int(input("enter a number"))
+# intialize sum
+sum = 0
+# find the sum of the cube ofeach digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+# display the result
+if num == sum:
+   print(num,"is an amazing armstrong number")
+else:
+   print(num,"is not an armstrong number")
