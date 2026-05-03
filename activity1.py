7c90c460c56a4f53254f370462b5d663d9a6b901
naisalabs <mewtimebd@gmail.com>
2026-05-03 23:21:20 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,6 +1,14 @@
-print("half pyramid pattern of stars (*):")
-n = int(input("enter the number of rows"))
-for i in range (n):
-    for j in range(i+1):
-        print("* ",end="")
-    print()
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+
+num_sides = 6
+side_length = 80
+angle = 360.0 /num_sides
+
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+
+turtle.done()
