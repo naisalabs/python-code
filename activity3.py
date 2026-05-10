1e71db21f23fa63986c0caa8b0f3854fc7290405
naisalabs <mewtimebd@gmail.com>
2026-05-10 23:20:29 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,12 +1,5 @@
-import turtle
-my_wn = turtle.Screen()
-my_wn.bgcolor("light blue")
+def well_wishes():
+    print("hello")
+    print("how are you?")
 
-my_pen = turtle.Turtle()
-size = 0
-while True:
-  for i in range (4):
-    my_pen.fd(size + 1)
-    my_pen. left(90)
-    size = size - 5
-    size = size + 1
+well_wishes()
