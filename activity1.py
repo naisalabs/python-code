610b7d1157c78265f59cd7c82482174b5f2ed9fd
naisalabs <mewtimebd@gmail.com>
2026-05-12 21:04:08 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,14 +1,11 @@
-import turtle
-turtle.Screen().bgcolor("green")
-turtle.Screen().setup(300,400)
-polygon = turtle.Turtle()
-
-num_sides = 6
-side_length = 80
-angle = 360.0 /num_sides
-
-for i in range(num_sides):
-    polygon.forward(side_length)
-    polygon.right(angle)
-
-turtle.done()
+def factorial(x):
+   if x==0 or x== 1:
+       return 1
+   else:
+       #calling function inside a function
+       return x*factorial(x-1)
+print("the factorial of 0:",factorial(0))
+print("the factorial of 1:",factorial(1))
+print("the factorial of 2:",factorial(2))
+print("the factorial of 5:",factorial(5))
+print("the factorial of 10:",factorial(10))
