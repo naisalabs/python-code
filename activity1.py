b7c5fc2a77a6c05f8922dc813d06bf3467c760e4
naisalabs <mewtimebd@gmail.com>
2026-05-13 23:34:44 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,11 +1,8 @@
-def factorial(x):
-   if x==0 or x== 1:
-       return 1
-   else:
-       #calling function inside a function
-       return x*factorial(x-1)
-print("the factorial of 0:",factorial(0))
-print("the factorial of 1:",factorial(1))
-print("the factorial of 2:",factorial(2))
-print("the factorial of 5:",factorial(5))
-print("the factorial of 10:",factorial(10))
+var = 10
+while var > 0:
+   var = var - 1
+   if var == 5:
+      continue
+      #display result
+   print('\ncurrent variable value:',var)
+print("\nGOOD BYE!")
