301eb153c00f6fddf706a3197faa1eb7a35d969c
naisalabs <mewtimebd@gmail.com>
2026-05-13 23:15:52 +0600

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,7 +1,7 @@
-def weather_condition():
-    print('the weather is pleasent in:', spring)
-    print('the weather is same in:',autumn)
-
-spring = "autumn"
-autumn = spring
-weather_condition()
+a = input ("Enter a word: ")
+for i in a :
+    if  (i == 'A'):
+        print ("A in found")
+        break
+    else:
+        print("A is not found")
