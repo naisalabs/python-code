909ee26623f62e4037cff31e5cb65708cbaacbbe
naisalabs <mewtimebd@gmail.com>
2026-05-17 23:29:27 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,8 +1,20 @@
-var = 10
-while var > 0:
-   var = var - 1
-   if var == 5:
-      continue
-      #display result
-   print('\ncurrent variable value:',var)
-print("\nGOOD BYE!")
+try:
+    num1, num2 = eval(input("enter two numbers, separated by coma: "))
+    result = num1 / num2
+    print("result is",result)
+#using multiple except block for different type errr
+
+except ZeroDivisionError:
+    print("division by error is error")
+
+except SyntaxError:
+    print("coma is missing")
+
+except:
+    print("wrong input")
+
+else:
+    print("no exceptions")
+
+finally:
+    print("this will execute no matter what happend")
