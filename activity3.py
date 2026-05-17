5bea8ae4301f3de77159102815f709f2948ddb63
naisalabs <mewtimebd@gmail.com>
2026-05-17 23:48:31 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,15 +1,16 @@
-for x in range(10): 
-    if x % 20 == 0:
-       print("twist")
+try:
+    age = int(input("Enter your age: "))
 
-    elif x % 15 == 0:
-       pass
-    
-    elif x % 5 == 0:
-       print("fizz")
-    
-    elif x % 3 == 0:
-       print("buzz")
-
+    # Check if age is valid
+    if age <= 0 or age > 120:
+        print("Error: Invalid age entered.")
     else:
-       print("x")
+        print("Valid age entered.")
+
+        # Check even or odd
+        if age % 2 == 0:
+            print("The age is Even.")
+        else:
+            print("The age is Odd.")
+except ValueError:
+    print("Error: Please enter a valid numeric age.")
