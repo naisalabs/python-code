95acd2064797c7be4d7982064063b44b629f9756
naisalabs <mewtimebd@gmail.com>
2026-06-04 23:22:39 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,9 +1,16 @@
-try:
-    age = int(input("enter a age: "))
-    if(age<18):
-        raise ValueError
+import random #importing module
+playing =True #initialise
+number = str(random.randint(0,10))
+
+print("i will generate a number from a number from 0 to 10 and you have to guess the number one digit at a time.")
+print("the game ends when you get 1 hero")
+
+while playing:
+    guess = input("give me your best guess! \n")
+    if number == guess:
+        print("you win the game")
+        print("the number was",number)
+        break
+
     else:
-        print("the age is valid")
-    
-except ValueError:
-    print("the age is not valid")
+        print("your guess isn't quite right, try again\n")
