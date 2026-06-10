afa3b4240755ff35af600f55eb4c604d233d9926
naisalabs <mewtimebd@gmail.com>
2026-06-10 23:19:55 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,7 +1,15 @@
-#using try and except
-try:
-    number = int(input("enter a number:"))
-    print("the number entered is",number)
-#using value error
-except ValueError as ex:
-    print("exception:",ex)
+#create and empty list
+empty_list = []
+print("empty_list")
+
+#a list of numbers
+numbers = [1, 2, 3, 4, 5,]
+print(numbers)
+
+#use * operator
+triples = [1, 2, 3,] * 3
+print(triples)
+# reverse rthe given list
+aList = [100, 200, 300, 400, 500,]
+aList = aList[::-1]
+print(aList,"\n")
