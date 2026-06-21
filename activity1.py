a35c76438c8b7b7e25afbfe5c2607622f9577652
naisalabs <mewtimebd@gmail.com>
2026-06-21 23:38:57 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,11 +1,9 @@
-def multiply_tuple(numbers):
-    product = 1
-    for num in numbers:
-        product *= num
-        return product
-    
-t = (2,3,4,5)
+test_dict = {'codingal' : 1, 'is' : 1, 'best' :3, 'for' : 8, 'coding' : 1}
+print("the original dictonary : " + str(test_dict))
+K = 1
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
 
-result = multiply_tuple(t)
-print("product of tuple elements:",result)
-
+print("frequency of K is: " + str(res))
