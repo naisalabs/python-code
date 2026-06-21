8b4df16d7d3674f709317ea0bc65efabcd1ee624
naisalabs <mewtimebd@gmail.com>
2026-06-21 23:29:16 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,13 +1,17 @@
-weather=(1,0,0,0,1,1,0)
-sunny=0
-rainy=1
-for i in range(0,7):
-    if(weather[i]==0):
-        rainy+=1
-    else:
-        sunny+=1
-
-if(sunny>rainy):
-    print("good weather")
-else:
-    print("bad weather")
+student_data = {
+    "id1": {"name": "naisa", "class": "IV", "subject_integation": "english"},
+    "id2": {"name": "sara", "class": "V", "subject_integation": "math"},
+    "id3": {"name": "david", "class": "III", "subject_integation": "science"},
+    "id4": {"name": "surya", "class": "I", "subject_integation": "islamic_studies"},
+}
+result = {}
+seen_keys = []
+for student_id,details in student_data.items():
+    unique_key = (details["name"], details["class"],
+        details["subject_integation"])
+    if unique_key not in seen_keys:
+        seen_keys.append(unique_key)
+        result[student_id] = details
+        #print output line by line
+        for k, v in result.items():
+            print(k,":", v)
