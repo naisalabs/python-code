f0d6b7aaaf5e08937cf9365c667d804ebeb70daf
naisalabs <mewtimebd@gmail.com>
2026-06-24 23:45:54 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,17 +1,9 @@
-student_data = {
-    "id1": {"name": "naisa", "class": "IV", "subject_integation": "english"},
-    "id2": {"name": "sara", "class": "V", "subject_integation": "math"},
-    "id3": {"name": "david", "class": "III", "subject_integation": "science"},
-    "id4": {"name": "surya", "class": "I", "subject_integation": "islamic_studies"},
-}
-result = {}
-seen_keys = []
-for student_id,details in student_data.items():
-    unique_key = (details["name"], details["class"],
-        details["subject_integation"])
-    if unique_key not in seen_keys:
-        seen_keys.append(unique_key)
-        result[student_id] = details
-        #print output line by line
-        for k, v in result.items():
-            print(k,":", v)
+set1 = set(map(int, input("enter elemnts of set 1 separated by spaces: "
+    ).split()))
+
+set2 = set(map(int, input("enter elemnts of set 2 separated by spaces: "
+    ).split()))
+
+result = set1.symmetric_difference(set2)
+
+print("symmetric Different:", result)
