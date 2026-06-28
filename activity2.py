200606d332f376b6b6eb9d3cf6a87778e15167fa
naisalabs <mewtimebd@gmail.com>
2026-06-28 23:31:09 +0600

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,8 +1,6 @@
-setx = {"green", "blue"}
-sety = {"blue","yellow"}
-print("original set elements: ")
-print("setx")
-print("sety")
-print("\nIntersection of two said sets: ")
-setz = setx.intersection(sety)
-print(setz)
+for i in range(10):
+
+    if i == 5:
+        print(exit)
+        exit()
+    print(i)
