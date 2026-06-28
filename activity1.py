66de23b455ca5574a61f06f306e54d1b968b1868
naisalabs <mewtimebd@gmail.com>
2026-06-28 22:38:26 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,11 +1,10 @@
 import array as arr
 
-array_num = arr.array('i', [1, 3, 5, 3, 7, 9, 8])
-print("original array: "+ str(array_num))
+array_num = arr.array('i',[1,2,5,3,7,5])
+print("original array: "+str(array_num))
 
-print("number of occurrences of the number 3 in the said array: "+str
-    (array_num.count(3)))
+print("number of occurrences of the number 3 in the said array: "+str(array_num.count(3)))
 
 array_num.reverse()
 print("reverse the order of the items:")
-print("str(array_num)")
+print(str(array_num))
