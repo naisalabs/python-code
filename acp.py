058c3677af274f4a96514a68495c1b898d67d0a2
naisalabs <mewtimebd@gmail.com>
2026-06-29 20:56:02 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,11 +1,14 @@
-numbers1 = [1,2,3]
-numbers2 = [4,5,6]
-result = map(lambda x,y: x + y, numbers1,numbers2)
-print("additon of two lists")
+# Add two lists using map and lambda
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print("Addition of two lists")
 print(list(result))
 
-nums = [1,2,3,4,5]
-def sq(n):
-    return n*n
+#using map
+nums = [1, 2, 3, 4, 5]  
+def sq(n):    
+    return n*n  
 square = list(map(sq, nums))
+print("Square of numbers in list")
 print(square)
