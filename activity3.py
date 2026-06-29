9b40e27fea68117ff889a678fc5c9ff7546d0ccb
naisalabs <mewtimebd@gmail.com>
2026-06-29 19:07:02 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,11 +1,18 @@
-my_set = {1, 2, 3}
-print(my_set)
-my_set = {1,2,3,4,3,2}
-print("my_set")
-my_set = ([1, 2, 3, 4, 5])
-print(my_set,"\n")
-num_set = set([0, 1, 3, 4, 5])
-print(num_set)
-num_set.pop()
-print("after removing the first element from the said set:")
-print(num_set,"\n")
+numbers = [1,2,3,4,5,6,7,8,9,10]
+squares = [x**2 for x in numbers]
+cubes = [x**2 for x in numbers]
+evens = [x for x in numbers if x % 2 == 0]
+odds = [x for x in numbers]
+greater_than_five = [x for x in numbers if x > 5]
+double_values = [x * 2 for x in numbers]
+half_values = [x / 2 for x in numbers]
+print("original:",numbers)
+print("squres:",squares)
+print("cubes:",cubes)
+print("even:",evens)
+print("odd:",odds)
+print("greater than 5:",greater_than_five)
+print("double:", double_values)
+print("half:",half_values)
+print("total numbers:",len(numbers))
+print("done!")
