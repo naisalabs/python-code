85151443ea48246c93741bad3b58d4d1ef077610
naisalabs <mewtimebd@gmail.com>
2026-07-05 23:45:57 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,14 +1,18 @@
-# Add two lists using map and lambda
-numbers1 = [1, 2, 3]
-numbers2 = [4, 5, 6]
-result = map(lambda x, y: x + y, numbers1, numbers2)
-print("Addition of two lists")
-print(list(result))
+class Dog:
+    animal = "dog"
 
-#using map
-nums = [1, 2, 3, 4, 5]  
-def sq(n):    
-    return n*n  
-square = list(map(sq, nums))
-print("Square of numbers in list")
-print(square)
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
+
+    def display(self):
+        print("animal:",Dog.animal)
+        print("breed:",self.breed)
+        print("name:",self.name)
+        print()
+
+dog1 = Dog("labrador","buddy")
+dog2 = Dog("german shephered","rocky")
+
+dog1.display()
+dog2.display()
