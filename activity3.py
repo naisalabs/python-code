4dd9448a65656f561d2f54e7cdba6c5240eac0c8
naisalabs <mewtimebd@gmail.com>
2026-07-05 23:11:02 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,18 +1,7 @@
-numbers = [1,2,3,4,5,6,7,8,9,10]
-squares = [x**2 for x in numbers]
-cubes = [x**2 for x in numbers]
-evens = [x for x in numbers if x % 2 == 0]
-odds = [x for x in numbers]
-greater_than_five = [x for x in numbers if x > 5]
-double_values = [x * 2 for x in numbers]
-half_values = [x / 2 for x in numbers]
-print("original:",numbers)
-print("squres:",squares)
-print("cubes:",cubes)
-print("even:",evens)
-print("odd:",odds)
-print("greater than 5:",greater_than_five)
-print("double:", double_values)
-print("half:",half_values)
-print("total numbers:",len(numbers))
-print("done!")
+# create a class
+class student:
+    grade = 4
+    print("Hi, iam a student of grade",grade)
+
+#create an object
+ob = student
