67d8fa360bce3364e1dbfbe52645bfd739c8b1af
naisalabs <mewtimebd@gmail.com>
2026-07-12 23:33:27 +0600

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,15 +1,22 @@
-class IOstring():
+class bird:
+    def __init__(self):
+        print("bird is ready")
+    def whoisThis(self):
+        print("bird")
+    def swim(self):
+        print("swim faster")
 
-     #constructor to set default value
-     def __init__(self):
-          self.str1 = ""
-    #function to print the string in upper case
-     def get_string(self):
-          self.str1 = input("Enter string")
-     def print_string(self):
-          print("result is :",self.str1.upper())
-# object creation
-str1 = IOstring()
-#call functions
-str1.get_string()
-str1.print_string()
+class penguin(bird):
+    def __init__(self):
+
+        super().__init__()
+        print("penguin is ready")
+    def whoisThis(self):
+        print("penguin")
+    def run(self):
+        print(":run faster")
+#object creation
+peggy = penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
