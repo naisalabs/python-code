6c5e68469202c86ea158ac4202a2275a0e5598fb
naisalabs <mewtimebd@gmail.com>
2026-07-17 22:13:33 +0600

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,18 +1,15 @@
-class Dog:
-    animal = "dog"
+class Vehicle:
+    def __init__(self, fare):
+        self.fare = fare
 
-    def __init__(self, breed, name):
-        self.breed = breed
-        self.name = name
 
-    def display(self):
-        print("animal:",Dog.animal)
-        print("breed:",self.breed)
-        print("name:",self.name)
-        print()
+class Bus(Vehicle):
+    def __init__(self, fare):
+        super().__init__(fare)
+        self.total_fare = fare + (fare * 10 / 100)   # 10% extra fare
 
-dog1 = Dog("labrador","buddy")
-dog2 = Dog("german shephered","rocky")
 
-dog1.display()
-dog2.display()
+bus = Bus(500)
+
+print("Base fare:", bus.fare)
+print("Total fare:", bus.total_fare)
